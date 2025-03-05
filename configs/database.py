f617e69b26b2eb6ab8a20d6c4454b42577c8b169
Remist9 <123454321Mazefaka>
2025-03-05 10:55:17 +0700

    V.007 ---

--- configs/database.py
+++ configs/database.py
@@ -1,9 +1,10 @@
-from clickhouse_driver import Client
+import aiochclient
 
-client = Client(
-    host='localhost',  # Адрес сервера
-    port=9000,         # Порт ClickHouse (по умолчанию 9000 для TCP)
-    user='user',    # Имя пользователя
-    password='user',       # Пароль (если есть)
-    database='ai'  # База данных
-)
+
+async def client():
+    return aiochclient.Client(
+        url='http://localhost:8123',
+        user='user',
+        password='user',
+        database='ai'
+    )
